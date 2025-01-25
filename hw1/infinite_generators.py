ee6758f58f844b9f3db4b35acda8fd674b6b4bf6
Daniel Stone <stoned12@my.erau.edu>
2025-01-25 12:02:26 -0700

    need to add assert statements

--- hw1/infinite_generators.py
+++ hw1/infinite_generators.py
@@ -8,33 +8,72 @@ while the second generates a "look-say" sequence, where the numbers you read and
 printed in the next sequence.
 
 For example, 1 -> "one 1" -> 11 -> "two ones" -> 21 -> one two and one one -> 1211
+
+Help: Help from Chat GPT debugging conditions for while loop and location of yield statement
 """
 from typing import Generator
 
-def fibonacci(*args) -> Generator(int, None, None):
+def fibonacci(n: int = None) -> Generator[int, None, None]:
     """
     Generates infinite fibonacci sequence.
     
     Arguments: 
-                *args is an optional argument for the number of terms to be generated
+                n is an optional argument for the number of terms to be generated
 
     Yields: 
                 int: next number in the fibonacci sequence
     """
-    #code here
+    n1, n2 = 0, 1
+    count = 0
 
-def look_say(*args) -> Generator(int, None, None):
+    while n is None or count < n:
+
+        yield n1
+        n1, n2 = n2, n1 + n2
+        count += 1
+
+
+
+def look_say(n: int = None) -> Generator[str, None, None]:
     """
     Generates infitine look-say sequence.
 
     Arguments:
-                *args is an optional argument for the number of terms to be generated
+                n is an optional argument for the number of terms to be generated
 
     Yields: 
                 int: next number in the fibonacci sequence
 
     Example:
                 1, 11, 21, 1211, 111221, and so on
+"""
+    loop_counter = 0 # counter for how many terms need to be generated
+    num = "1" # current value in sequence
 
-    """
-    #code here
+
+    while n is None or loop_counter < n:
+
+        yield num
+        num_counter = 1 # counter for current letter
+        last_char = num[0] # stores previous value of num string
+        ans = "" # working value to concatenate values as num is iterated over
+
+        # iterate over current num, starting at second character in the string
+        for current_char in num[1:]:
+            if current_char == last_char:
+                num_counter += 1 # increment counter is current_char same as last_char
+            else:
+                ans += str(num_counter) + last_char # append counter of character and character to ans
+                num_counter = 1 # reset counter for the next letter
+            last_char = current_char #update last_char
+
+        ans += str(num_counter) + last_char # append last counter of character and character to ans
+        num = ans # update num
+        loop_counter += 1 # increment while loop counter
+
+assert list(fibonacci(10)) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+assert list(fibonacci(-1)) == []
+assert list(fibonacci(0)) == []
+assert list(fibonacci(1)) == [0]
+assert list(fibonacci(2)) == [0, 1]
+
